f358ef912b66751060e48c4b315b69b2b72fe42d
Budvin Chathura <chathura97rc@gmail.com>
2020-02-13 03:34:34 +0530

    Updates

---
+++ precache-manifest.1c3587c82ef2e8ac0b44256f4a2c6f02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788c28fb95c1a8ecfcdfdf421630162a",
+    "url": "/leaflet-test/index.html"
+  },
+  {
+    "revision": "f119fb0a9e4d363d4681",
+    "url": "/leaflet-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "145aacd45efb07546b2e",
+    "url": "/leaflet-test/static/js/2.0cb2224c.chunk.js"
+  },
+  {
+    "revision": "a108bd14c69faf1507e1074a6c8a5de4",
+    "url": "/leaflet-test/static/js/2.0cb2224c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f119fb0a9e4d363d4681",
+    "url": "/leaflet-test/static/js/main.b4f59e59.chunk.js"
+  },
+  {
+    "revision": "0d22ad87acbce935a743",
+    "url": "/leaflet-test/static/js/runtime-main.cc86fe1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leaflet-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
