6ebef980bb0586abfd3e6eb53f2db88f5e7b6245
Budvin Chathura <chathura97rc@gmail.com>
2020-04-19 01:21:18 +0530

    markers state fix

--- src/redux/reducers/markers.js
+++ src/redux/reducers/markers.js
@@ -1,11 +1,11 @@
 const markers_reducer = (markers = {}, action) => {
     switch (action.type) {
         case 'SET_A':
-            return Object.assign({markers},{a:action.payload})
+            return Object.assign({},markers,{a:action.payload})
         case 'SET_B':
-            return Object.assign({markers},{b:action.payload})
+            return Object.assign({},markers,{b:action.payload})
         case 'SET_C':
-            return Object.assign({markers},{b:action.payload})
+            return Object.assign({},markers,{b:action.payload})
         default:
             return markers
     }
